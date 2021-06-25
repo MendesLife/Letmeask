6e2bbe28ae4ef2e17db210721ec730af7f5f580c
Victor Mendes <victormendes08@hotmail.com>
2021-06-24 22:14:25 -0300

    Projeto nlw letmeask

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,108 @@
+
+import {useHistory, useParams} from 'react-router-dom'
+import checkImg from "../assets/images/check.svg"
+import answerImg from "../assets/images/answer.svg"
+import deleteImg from '../assets/images/delete.svg'
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import { Question } from '../components/Question';
+import { RoomCode } from '../components/RoomCode';
+//import { useAuth } from '../hooks/useAuth';
+import { useRoom } from '../hooks/useRoom';
+import { database } from '../services/firebase';
+import '../styles/room.scss';
+
+
+
+type RoomParams = {
+    id:string;
+}
+
+
+export function AdminRoom(){
+// const {user} = userAuth();
+const history =useHistory()
+const params = useParams<RoomParams>();
+const roomId = params.id;
+
+const {title, questions} = useRoom(roomId)
+
+async function handleEndRoom() {
+   await database.ref(`rooms/${roomId}`).update({endedAt: new Date(),
+})
+  
+history.push('/');
+}
+
+
+async function handleDeleteQuestion(questionId: string){
+    if (window.confirm("Você tem certeza que deseja excluir essa pergunta?")){
+         await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+}
+
+async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({isAnswered: true});
+}
+
+        
+        
+
+async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+        isHighLighted: true,
+    });
+        }
+
+    return(
+        <div id="page-room">
+            <header>
+<div className="content">
+    <img src={logoImg} alt="Letmeask" />
+    <div>
+    <RoomCode code={params.id}/>
+    <Button isOutlined onClick={handleEndRoom}>Encerrar Sala</Button>
+    </div>
+</div>
+            </header>
+            <article className="main-room">
+                <div className="room-title">
+                    <h1>Sala {title}</h1>
+                    {questions.length > 0 && <span>{questions.length} pergunta(s)</span>} 
+                </div>
+                <div className="question-list">
+                {questions.map(question => {
+                    return(
+                    <Question
+                    key={question.id}
+                    content={question.content}
+                    author={question.author}
+                    isAnswered={question.isAnswered}
+                    isHighLighted={question.isHighLighted}>
+                        {!question.isAnswered && (
+                            <>
+                            <button
+                            type="button"
+                            onClick={() => handleCheckQuestionAsAnswered(question.id)}>
+                                <img src={checkImg} alt="Marcar a mensagem" />
+                            </button>
+                            <button
+                            type="button"
+                            onClick={() => handleHighlightQuestion(question.id)}>
+                                <img src={answerImg} alt="Dar destaque a mensagem" />
+                            </button>
+                            </>
+                        ) }
+                        <button
+                        type="button"
+                        onClick={() => handleDeleteQuestion(question.id)}>
+                            <img src={deleteImg} alt="Deletar mensagem" />
+                        </button>
+                    </Question>
+                );
+                })}
+                </div>
+            </article>
+        </div>
+    )
+}
